b8ba97f9e28aba266de2d6b13c748cf919808468
Digital Exploitation of Children Task Force <56242810+dectaskforce@users.noreply.github.com>
2019-10-18 16:58:56 -0400

    Add files via upload

--- rsakiller.py
+++ rsakiller.py
@@ -1,25 +1,24 @@
 '''
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-#  rsakiller.py
-#  
-#  Copyright October 2019 sixie6e@dectaskforce
-#  
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#  
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
+
+ rsakiller.py
+ 
+ Copyright October 1, 2019 sixie6e@dectaskforce
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ MA 02110-1301, USA.
 '''  
 
 from prime_list import prime_search, largest_prime, set_maths
@@ -27,7 +26,6 @@ from prime_list import prime_search, largest_prime, set_maths
 #print(prime_search())
 #print(largest_prime(46357573574653))
 
-#print(f'{rsa:,d}')
-
-
 print(set_maths())
+
+#print(f'{rsa:,d}')
